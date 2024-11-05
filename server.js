c12d24307eb5f2ac7da20d3429b07bdedb040bc5
zami-25 <aamirhosseinzz.25@gmail.com>
2024-11-05 16:00:43 +0330

    test server3

--- server.js
+++ server.js
@@ -9,19 +9,33 @@ var mongo =require('mongodb')
 var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/mydb";
 
-
-MongoClient.connect(url,
-   function(err, db)
-    {
+app.get('/',(req,res) => {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		console.log("Database created!");
+		db.close();
+	  })
+})
+MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-  	var dbo = db.db("mydb");
-  	dbo.createCollection("customers", function(err, res)
-		{
-    	if (err) throw err;
-    	console.log("Collection created!");
+	console.log("Database created!");
+	db.close();
+  });
+
+
+
+// MongoClient.connect(url,
+//    function(err, db)
+//     {
+// 	if (err) throw err;
+//   	var dbo = db.db("mydb");
+//   	dbo.createCollection("customers", function(err, res)
+// 		{
+//     	if (err) throw err;
+//     	console.log("Collection created!");
   
-  		db.close();
-		});
+//   		db.close();
+// 		});
 		// insert obj to customers collection(table)
 		// var myobj = { name: "Company Inc", address: "Highway 37" };
 		// dbo.collection("customers").insertOne(myobj, function(err, res) {
@@ -29,7 +43,7 @@ MongoClient.connect(url,
 		//   console.log("1 document inserted");
 		//   db.close();
 	
-	})
+	
 //app.use(express.static("public"))
 //app.set("view engine", "ejs")
 
