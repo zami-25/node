8b5e48816dea4db29fbc7420f4c880896be169d9
zami-25 <aamirhosseinzz.25@gmail.com>
2024-11-05 12:31:45 +0330

    conect mongo db

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const app = express()
 const port = 3000
 app.use(express.urlencoded({extended:true}))
 
+var mongo =require('mongodb')
+var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/mydb";
+
+
+MongoClient.connect(url,
+   function(err, db)
+    {
+	if (err) throw err;
+  	var dbo = db.db("mydb");
+  	dbo.createCollection("customers", function(err, res)
+		{
+    	if (err) throw err;
+    	console.log("Collection created!");
+  
+  		db.close();
+		});
+		// insert obj to customers collection(table)
+		// var myobj = { name: "Company Inc", address: "Highway 37" };
+		// dbo.collection("customers").insertOne(myobj, function(err, res) {
+		//   if (err) throw err;
+		//   console.log("1 document inserted");
+		//   db.close();
+	})
+
 //app.use(express.static("public"))
 //app.set("view engine", "ejs")
 
