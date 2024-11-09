37a79c746cf0c9623ad05a9c21b5f5184d4b8206
zami-25 <aamirhosseinzz.25@gmail.com>
2024-11-09 15:48:09 +0330

    isnert in data base

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router=express()
 router.set("view engine", "ejs")
 //router.use(express.urlencoded({extended:true}))
 //router.use(logg)
-
+ var  MongoClient = require('mongodb').MongoClient;
 
 
 router.get('/',
@@ -19,20 +19,47 @@ router.get('/new',
 
 })
 
+//router.post('/new',
+  //  (req,res)=>{
+   // const isValid=true
+   // if(isValid){
+     // users.push({name:req.body.name})
+     // console.log(users)
+     // res.redirect(`/useres/${users.length-1}`)
+    //}else{
+      //console.log("error")
+     // res.render('users/new',{name:req.body.name
+     // })
+   // }
+ //} )
+
 router.post('/new',
-    (req,res)=>{
-    const isValid=true
-    if(isValid){
-      users.push({name:req.body.name})
-      console.log(users)
-      res.redirect(`/useres/${users.length-1}`)
-    }else{
-      console.log("error")
-      res.render('users/new',{name:req.body.name
-      })
+(req,res)=>{
+
+const client = new MongoClient("mongodb://admin:pass@localhost:27017");
+async function run() {
+  try {
+
+    const database = client.db("test");
+    const users = database.collection("users");
+
+
+    const doc = {
+      name:req.body.name
     }
- } )
-    
+
+    const result = await users.insertOne(doc);
+
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+});
+
 
 router.route("/:id")
 .delete
@@ -59,19 +86,19 @@ router.route("/:id")
 )
 
 
-const users=[{name:'amir'},{name:'alo'}]
-router.param("id",(req,res,next,id)=>{
-    req.user=users[id]
-    next()
+//const users=[{name:'amir'},{name:'alo'}]
+//router.param("id",(req,res,next,id)=>{
+  //  req.user=users[id]
+    //next()
     // console.log(users[1])
     // res.send(`$hello ${users[1]}`)
 
 
-})
-function logg(req, res,next){
-    console.log(req.url)
-    next()
-  }
+//})
+//function logg(req, res,next){
+  //  console.log(req.url)
+    //next()
+ // }
     
 
 
