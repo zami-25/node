377d929ea590203e9ea7639c04b030e526f14c1b
zami-25 <aamirhosseinzz.25@gmail.com>
2024-11-11 15:22:54 +0330

    add data base to login

--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,15 @@ const express = require('express')
 const login=express()
 const path = require('path')
 login.use(express.urlencoded({extended:true}))
+
+
 login.use('/js', express.static(path.join(__dirname, 'public/js')))
 login.use('/css', express.static(path.join(__dirname, 'public/cdn/css')))
 login.use('/jquery', express.static(path.join(__dirname, 'public/cdn/jquery')))
 
 login.set("view engine", "ejs")
+
+var  MongoClient = require('mongodb').MongoClient;
 
 login.get('/',(req,res)=>{
     res.render("login/index")
@@ -15,9 +19,32 @@ login.get('/',(req,res)=>{
 
  login.post('/',(req,res)=> 
  {
-   console.log(req.body.username)
-   console.log(req.body.password)
- })
+
+  const client = new MongoClient("mongodb://admin:pass@localhost:27017");
+async function run() {
+  try {
+
+    const database = client.db("test");
+    const users = database.collection("users");
+
+
+    const doc = {
+      name:req.body.username,
+      password:req.body.password
+    }
+
+    const result = await users.insertOne(doc);
+
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+});
+
   
 
 module.exports=login
